7089bdda37f952ad4dee79369ff82eb69e083960
JC <juancfoofn@yahoo.com>
2024-01-04 10:37:04 +0100

    Ex06 ('ft_print_alphabet.c') done

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 10:27:59 by jfidalgo          #+#    #+#             */
+/*   Updated: 2024/01/04 10:35:13 by jfidalgo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write (1, &letter, 1);
+		letter++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
