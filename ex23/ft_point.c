86e42f1e2d010107dd4ec08baf20625747ddee0f
JC <juancfoofn@yahoo.com>
2024-01-07 21:05:43 +0100

    Ex23 ('ft_point') done

---
+++ ex23/ft_point.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 20:47:32 by jfidalgo          #+#    #+#             */
+/*   Updated: 2024/01/07 20:58:54 by jfidalgo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+#include <stdio.h>
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int	main(void)
+{
+	t_point	point;
+
+	set_point(&point);
+	printf("X: %d\n", point.x);
+	printf("Y: %d\n", point.y);
+	printf("\n");
+	return (0);
+}
