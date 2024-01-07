b5c18f7e36d63b26d6cd240530bea368218b570d
JC <juancfoofn@yahoo.com>
2024-01-07 09:45:35 +0100

    Ex20 ('ft_strdup') done

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 09:16:47 by jfidalgo          #+#    #+#             */
+/*   Updated: 2024/01/07 09:42:00 by jfidalgo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+// #include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	value;
+
+	value = 0;
+	while (str[value] != '\0')
+		value++;
+	return (value);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*result;
+	int		i;
+
+	result = malloc(ft_strlen(src) + 1);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		result[i] = src[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+/*
+int	main(void)
+{
+	char	*dup;
+
+	dup = ft_strdup("GOTT");
+	printf("%s", dup);
+	free(dup);
+}
+*/
